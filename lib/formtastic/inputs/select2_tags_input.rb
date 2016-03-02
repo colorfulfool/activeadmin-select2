7d8804c3771cab09899d3f8b6b34faffdbb37eda
Anton Khamets <t0ny.hamster@gmail.com>
2016-03-03 00:27:07 +0300

    Allow user to type his own options in select2_tags

--- lib/formtastic/inputs/select2_tags_input.rb
+++ lib/formtastic/inputs/select2_tags_input.rb
@@ -6,8 +6,8 @@ module Formtastic
     class Select2TagsInput < Formtastic::Inputs::StringInput
       def input_html_options
         {
-            class: 'select2-input select2-tags-input',
-            data: { select2: { tags: options[:collection] }}
+            class: 'select2-input', multiple: true,
+            data: { tags: true }
         }.merge(super)
       end
     end
