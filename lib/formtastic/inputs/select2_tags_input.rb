5b91abba2784a0d2a377a0a8441c148806f718c2
Anton Khamets <t0ny.hamster@gmail.com>
2016-03-03 00:42:31 +0300

    Oh, come on!

--- lib/formtastic/inputs/select2_tags_input.rb
+++ lib/formtastic/inputs/select2_tags_input.rb
@@ -6,8 +6,7 @@ module Formtastic
     class Select2TagsInput < Formtastic::Inputs::StringInput
       def input_html_options
         {
-            class: 'select2-input', multiple: true,
-            data: { tags: true }
+            class: 'select2-input select2-tags', multiple: true
         }.merge(super)
       end
     end
